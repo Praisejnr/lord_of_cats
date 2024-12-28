e77487a95c70e83a9ab4f7fb656bd107225f03ec
Praisejnr <praiseomoniyi65@gmail.com>
2024-12-28 03:18:34 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+function toggleBtn() {
+  var hamburger = document.querySelector(".hamburger");
+  var navBar = document.querySelector(".nav-bar");
+
+  hamburger.classList.toggle("active");
+  navBar.classList.toggle("active");
+}
